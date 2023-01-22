11aa387f0a9cb545831fa1ca0821f3ffad114d21
azopo <alexhorb@icloud.com>
2023-01-22 13:47:48 +0200

    add close icon component;

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@
     <!--    <AppHeader />-->
     <!--    <custom-button>Test</custom-button>-->
     <!--    <custom-input label="Card number" />-->
-    <custom-divider />
+    <!--    <custom-divider />-->
+    <close-icon />
   </div>
 </template>
 <script setup>
 // import CustomButton from "@/components/CustomButton.vue";
 // import AppHeader from "@/components/AppHeader.vue";
 //import CustomInput from "@/components/CustomInput.vue";
-import CustomDivider from "@/components/CustomDivider.vue";
+//import CustomDivider from "@/components/CustomDivider.vue";
+import CloseIcon from "@/components/CloseIcon.vue";
 </script>
