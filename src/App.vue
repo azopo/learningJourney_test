07edcc40b495a58196686ed76ff44e1ad50b40a5
azopo <alexhorb@icloud.com>
2023-01-23 17:01:24 +0200

    change bg image to webp;

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative h-screen">
     <div
-      class="absolute w-full h-full top-0 left-0 bg-[url('src/assets/img/bg.png')] opacity-[0.04] bg-fixed bg-center bg-cover bg-no-repeat z-[-1]"
+      class="absolute w-full h-full top-0 left-0 bg-[url('src/assets/img/bg.webp')] opacity-[0.04] bg-fixed bg-center bg-cover bg-no-repeat z-[-1]"
     />
     <header>
       <app-header />
